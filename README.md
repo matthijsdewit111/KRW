595d23bc84e8025def54bad4e791a6e43cb2db99
Benjamin Wang <wlongxiang1119@gmail.com>
2020-05-13 22:53:40 +0200

    Add WIP

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # KRW
-code for KRW project, potentially some sparql queries for grlc git api as well.
+
+## Getting started
+
+Run `get_triple.py` to fetch all related triples and convert it into a dummy graph
+
+Run `load_turtle.sh` to load triples into a GraphDB (make sure you have a GraphDB repo named `myrepo`)
--- conceptnet-test.py
+++ conceptnet-test.py
@@ -1,33 +1,21 @@
 import requests
-from rdflib import Graph, ConjunctiveGraph, Literal, BNode, Namespace, RDF, URIRef
 
-entity_examples = ["car", "person", "bike", "house", "motorcycle", "scooter", "street_lamp", "garbage_bag", "furniture",
-                   "christmas_tree", "mattress", "trash_bin", "container"]
-
-g = Graph()
-
-
-def add_triple(triple):
-    subject = URIRef("http://api.conceptnet.io{}".format(triple[0]))
-    predicate = URIRef("http://api.conceptnet.io{}".format(triple[1]))
-    object = URIRef("http://api.conceptnet.io{}".format(triple[2]))
-    g.add((subject, predicate, object))
-
+entity_examples = ["car", "person", "bike", "house", "motorcycle", "scooter", "street_lamp", "garbage_bag", "furniture", "christmas_tree", "mattress", "trash_bin", "container"]
 
 # get all (limited in this case to 100) relations of an entity
+# for entity in entity_examples:
+#     print(entity)
+#     obj = requests.get('http://api.conceptnet.io/c/en/{}?limit=100'.format(entity)).json()
+#     for edge in obj['edges']:
+#         print(edge['@id'])
+
+# find how much two entities are related (in this case determine how likely the entity is to be found inside or outside)
 for entity in entity_examples:
     print(entity)
-    obj = requests.get('http://api.conceptnet.io/c/en/{}?limit=100'.format(entity)).json()
-    for edge in obj['edges']:
-        triple = (edge["start"]["@id"], edge["rel"]["@id"], edge["end"]["@id"])
-        print("########start of statement########")
-        print(triple)
-        print("########end of statement########")
-        add_triple(triple)
-raw_ttl = g.serialize(format='turtle').decode("utf-8")
-print(raw_ttl)
-with open("example2.ttl", "w") as fp:
-    fp.write(raw_ttl)
+    obj = requests.get('http://api.conceptnet.io/relatedness?node1=/c/en/{}&node2=/c/en/find_inside'.format(entity)).json()
+    print("inside:", obj['value'])
+    obj = requests.get('http://api.conceptnet.io/relatedness?node1=/c/en/{}&node2=/c/en/find_outside'.format(entity)).json()
+    print("outside:", obj['value'])
 # find how much two entities are related (in this case determine how likely the entity is to be found inside or outside)
 # for entity in entity_examples:
 #     print(entity)
